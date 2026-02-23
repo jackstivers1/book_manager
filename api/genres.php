02ce2f7e947f785f1c8e9fdf711f1961eda7ed30
jackstivers1 <jstiver@clemson.edu>
2026-02-22 21:51:58 -0500

    Delete api directory

--- api/genres.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . "/db.php";
-
-$res = $conn->query("SELECT DISTINCT genre FROM books ORDER BY genre ASC");
-$genres = [];
-while ($row = $res->fetch_assoc()) {
-  $genres[] = $row["genre"];
-}
-echo json_encode(["genres" => $genres]);
